737d53a9f4b3836bede5e989cbbf5afa351dcaf0
Ofer goli <Ofergolib@gmail.com>
2015-03-08 19:23:03 +0200

    changes

---
+++ adsrv/ddls/data.js
@@ -0,0 +1,294 @@
+
+{
+  "2": {
+    "desc": "binary options",
+    "type": "ddls",
+    "rndrTyp": "img",
+    "ctgry": [
+      "forex"
+    ],
+    "cntry": [
+      "uk",
+      "za",
+      "au"
+    ],
+    "feed": "",
+    "standalone": false,
+    "source": [
+      "http://fmtrader.ck-cdn.com/tn/serve/?cid=386590"
+    ],
+    "lnk": [
+      "http://go.fmtrader.com/visit/?bta=35192&nci=5687ns"
+    ],
+    "id": 2
+  },
+  "3": {
+    "desc": "binary options arabic",
+    "type": "ddls",
+    "rndrTyp": "img",
+    "ctgry": [
+      "binary options"
+    ],
+    "cntry": [
+      "tn",
+      "ae",
+      "lb",
+      "sa"
+    ],
+    "feed": "",
+    "standalone": false,
+    "source": [
+      "http://fmtrader.ck-cdn.com/tn/serve/?cid=386710"
+    ],
+    "lnk": [
+      "http://go.fmtrader.com/visit/?bta=35192&nci=5642"
+    ],
+    "id": 3
+  },
+  "4": {
+    "desc": "binary options russia",
+    "type": "ddls",
+    "rndrTyp": "img",
+    "ctgry": [
+      "binary options"
+    ],
+    "cntry": [
+      "by",
+      "ru",
+      "ua"
+    ],
+    "feed": "",
+    "standalone": false,
+    "source": [
+      "http://fmtrader.ck-cdn.com/tn/serve/?cid=386726"
+    ],
+    "lnk": [
+      "http://go.fmtrader.com/visit/?bta=35192&nci=5583"
+    ],
+    "id": 4
+  },
+  "5": {
+    "desc": "binary options italy",
+    "type": "ddls",
+    "rndrTyp": "img",
+    "ctgry": [
+      "binary options"
+    ],
+    "cntry": [
+      "it"
+    ],
+    "feed": "",
+    "standalone": false,
+    "source": [
+      "http://fmtrader.ck-cdn.com/tn/serve/?cid=386732"
+    ],
+    "lnk": [
+      "http://go.fmtrader.com/visit/?bta=35192&nci=5677"
+    ],
+    "id": 5
+  },
+  "6": {
+    "desc": "888 sport Spain ",
+    "type": "ddls",
+    "rndrTyp": "img",
+    "ctgry": [
+      "Sports & Games Books",
+      "Magazines & Newspapers"
+    ],
+    "cntry": [
+      "es"
+    ],
+    "feed": "",
+    "standalone": false,
+    "source": [
+      "https://mmwebhandler.888.com/I/34482?sr=1130293"
+    ],
+    "lnk": [
+      "https://mmwebhandler.888.com/C/34482?sr=1130293&"
+    ],
+    "id": 6
+  },
+  "7": {
+    "desc": "miniplay",
+    "rndrTyp": "iframe",
+    "size": "300x250",
+    "ctgry": [
+      "Video Games"
+    ],
+    "cntry": [
+      "us"
+    ],
+    "domains": [],
+    "feed": "miniplay",
+    "standalone": true,
+    "source": [
+      "http://widgets.moosite.com/hot-games/?widget=Affiliate&p=eyJ1c2VySWQiOjYzLCJ0eXBlIjoiMzAweDI1MCAoQiBkYXJrKSIsIm1rdCI6IlpKQUZNT05USUVSQU1PTlRJRVJBIiwib3B0aW9uYWxQYXJhbXMiOnsibWt0YSI6IiIsImNzc0ZpbGUiOiIiLCJtb3JlVXJsIjoiIiwid2ViSWQiOiIxIiwic3NsIjpmYWxzZX19&s=745764f8c159d7f674d8bdcaa3016da0"
+    ],
+    "lnk": [],
+    "subidkey": "",
+    "subidval": "",
+    "id": "7"
+  },
+  "8": {
+    "desc": "plymedia_sterkly_entertainment",
+    "rndrTyp": "iframe",
+    "size": "300x250",
+    "ctgry": [
+      "Arts & Entertainment Books"
+    ],
+    "cntry": [],
+    "domains": [],
+    "feed": "",
+    "standalone": true,
+    "source": [
+      "http://cdn.mntrply.com/montiera/tags/xdirect/xdirect.html?p=57551492&serverdomain=montiera&size=300x250&ct=html&ap=1300"
+    ],
+    "lnk": [],
+    "subidkey": "",
+    "subidval": "",
+    "id": 8
+  },
+  "9": {
+    "desc": "plymedia_sterkly_sport",
+    "rndrTyp": "iframe",
+    "size": "300x250",
+    "ctgry": [
+      "Sports"
+    ],
+    "cntry": [],
+    "domains": [],
+    "feed": "",
+    "standalone": true,
+    "source": [
+      "http://cdn.mntrply.com/montiera/tags/xdirect/xdirect.html?p=57651116&serverdomain=montiera&size=300x250&ct=html&ap=1300"
+    ],
+    "lnk": [],
+    "subidkey": "",
+    "subidval": "",
+    "id": 9
+  },
+  "10": {
+    "desc": "plymedia_sterkly_music",
+    "rndrTyp": "iframe",
+    "size": "300x250",
+    "ctgry": [
+      "Music"
+    ],
+    "cntry": [],
+    "domains": [],
+    "feed": "plymedia",
+    "standalone": true,
+    "source": [
+      "http://cdn.mntrply.com/montiera/tags/xdirect/xdirect.html?p=58131656&serverdomain=montiera&size=300x250&ct=html&ap=1300"
+    ],
+    "lnk": [],
+    "subidkey": "",
+    "subidval": "",
+    "id": 10
+  },
+  "11": {
+    "desc": "plymedia_softonic_general",
+    "rndrTyp": "iframe",
+    "size": "300x250",
+    "ctgry": [],
+    "cntry": [],
+    "domains": [],
+    "feed": "plymedia",
+    "standalone": true,
+    "source": [
+      "http://cdn.mntrply.com/montiera/tags/xdirect/xdirect.html?p=58112653&serverdomain=montiera&size=300x250&ct=html&ap=1300"
+    ],
+    "lnk": [],
+    "subidkey": "",
+    "subidval": "",
+    "id": 11
+  },
+  "12": {
+    "desc": "plymedia_softonic_general ",
+    "rndrTyp": "iframe",
+    "size": "300x250",
+    "ctgry": [],
+    "cntry": [],
+    "domains": [],
+    "feed": "",
+    "standalone": true,
+    "source": [
+      "http://cdn.mntrply.com/montiera/tags/xdirect/xdirect.html?p=58112653&serverdomain=montiera&size=300x250&ct=html&ap=1300"
+    ],
+    "lnk": [],
+    "subidkey": "",
+    "subidval": "",
+    "id": 12
+  },
+  "13": {
+    "desc": "plymedia_softonic_general ",
+    "rndrTyp": "iframe",
+    "size": "300x250",
+    "ctgry": [],
+    "cntry": [],
+    "domains": [],
+    "feed": "",
+    "standalone": true,
+    "source": [
+      "http://cdn.mntrply.com/montiera/tags/xdirect/xdirect.html?p=58112653&serverdomain=montiera&size=300x250&ct=html&ap=1300"
+    ],
+    "lnk": [],
+    "subidkey": "",
+    "subidval": "",
+    "id": 13
+  },
+  "14": {
+    "desc": "plymedia_babylon_general",
+    "rndrTyp": "iframe",
+    "ctgry": [],
+    "feed": "",
+    "size": "300x250",
+    "domains": [],
+    "cntry": [],
+    "source": [
+      "http://cdn.mntrply.com/montiera/tags/xdirect/xdirect.html?p=58961556&serverdomain=montiera&size=300x250&ct=html&ap=1300"
+    ],
+    "lnk": [],
+    "standalone": false,
+    "prdct": "Babylon ",
+    "subidkey": "",
+    "subidval": "",
+    "id": 14
+  },
+  "15": {
+    "desc": "plymedia_babylon_general",
+    "rndrTyp": "iframe",
+    "ctgry": [],
+    "feed": "",
+    "size": "728x90",
+    "domains": [],
+    "cntry": [],
+    "source": [
+      "http://cdn.mntrply.com/montiera/tags/xdirect/xdirect.html?p=58961556&serverdomain=montiera&size=728x90&ct=html&ap=1300"
+    ],
+    "lnk": [],
+    "standalone": false,
+    "prdct": "Babylon ",
+    "subidkey": "",
+    "subidval": "",
+    "id": 15
+  },
+  "16": {
+    "desc": "plymedia_babylon_general",
+    "rndrTyp": "iframe",
+    "ctgry": [],
+    "feed": "",
+    "size": "160x600",
+    "domains": [],
+    "cntry": [],
+    "source": [
+      "http://cdn.mntrply.com/montiera/tags/xdirect/xdirect.html?p=58961556&serverdomain=montiera&size=160x600&ct=html&ap=1300"
+    ],
+    "lnk": [],
+    "standalone": false,
+    "prdct": "Babylon ",
+    "subidkey": "",
+    "subidval": "",
+    "id": 16
+  }
+}
