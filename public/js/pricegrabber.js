291ce7093fd5c3d6ae40f653b705dda885e2f8e8
Ofer goli <Ofergolib@gmail.com>
2015-03-26 16:52:03 +0200

    change img link

--- public/js/pricegrabber.js
+++ public/js/pricegrabber.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 					if($('#remove').length)
 						$('#remove').remove();
 					var index = findIndex(data.document.product);
-					var htm = "<div id='remove' style='text-decoration:none;position: fixed;bottom: -50px;left: 0px;width:300px;height:300px;cursor:pointer;background:url(\"http://localhost/images/t2.png\");background-repeat:no-repeat;' onclick='ui.bnrclk();'>" +
+					var htm = "<div id='remove' style='text-decoration:none;position: fixed;bottom: -50px;left: 0px;width:300px;height:300px;cursor:pointer;background:url(\"http://204.145.74.4/images/t2.png\");background-repeat:no-repeat;' onclick='ui.bnrclk();'>" +
 					"<a href='" + data.document.product[index].direct_offer.url + "' target=\"_blank\"><div style='width:152px;height:300px;float:left;'>" +
 					"<div style='width:150px;height:248px;float:left;cursor:pointer;'>" +
 					"<div id='txt1' style='padding-top:20px;padding-left:5px;color:#E8335D;font-family:arial,sans-serif;font-size:14px;height:14px;overflow:hidden;width:145px;text-align:center'>"+ data.document.product[index].catzero.$t +"</div>" +
