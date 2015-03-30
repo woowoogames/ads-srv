29eed8a62436a88910d123a5f70a831e28c7b15b
Ofer goli <Ofergolib@gmail.com>
2015-03-30 15:58:20 +0300

    priceGrabber fix ui and server

--- public/js/pricegrabber.js
+++ public/js/pricegrabber.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$("#bt_submit").click(function(){
 		$("#spinner").show();
-		//var host = "http://localhost"
-		var host = "http://204.145.74.4"
+		var host = "http://localhost"
+		//var host = "http://204.145.74.4"
 		var st  = $("#st").val();
 		$.ajax({
 			url : host + '/priceGrabberTestPage?search_kw=' + st,
@@ -17,16 +17,17 @@ $(document).ready(function(){
 					var htm = "<div id='remove' style='text-decoration:none;position: fixed;bottom: -50px;left: 0px;width:300px;height:300px;cursor:pointer;background:url(\"http://204.145.74.4/images/t2.png\");background-repeat:no-repeat;' onclick='ui.bnrclk();'>" +
 					"<a href='" + data.document.product[index].direct_offer.url + "' target=\"_blank\"><div style='width:152px;height:300px;float:left;'>" +
 					"<div style='width:150px;height:248px;float:left;cursor:pointer;'>" +
-					"<div id='txt1' style='padding-top:20px;padding-left:5px;color:#E8335D;font-family:arial,sans-serif;font-size:14px;height:14px;overflow:hidden;width:145px;text-align:center'>"+ data.document.product[index].catzero.$t +"</div>" +
-					"<div id='txt2' style='padding-left:5px;color:#E8335D;font-family:arial,sans-serif;font-size:24px;height:25px;overflow:hidden;width:145px;text-align:center'>" + data.document.product[index].direct_offer.price + "</div>" +
 					"<div style='width:128px;height:195px;float:left;padding-left:10px;'>" +
+					"<div style='width: 100px;height: 40px;margin-top:15px;margin-left:;font-size: 22px;color: rgb(253, 62, 96);;margin-left: 14px;/* text-align: center; */cursor:pointer;'>" + data.document.product[index].direct_offer.retailer + "</div>" +
 					"<img id='img1' src='" + data.document.product[index].image_large + "' style='width:130px;height:162px;margin-top:15px;cursor:pointer;' />" +
 					"</div>" +
 					"</div>" +
 					"</div>" +
 					"<div style='width:125px;height:300px;float:left;cursor:pointer;'>" +
-					"<div style='width:168px;height:248px;float:left;'>" +
-					"<div id='txt3' style='padding-left:12px;padding-top:10px;color:#f9f9f9;font-family:arial,sans-serif;font-size:17px;height:168px;overflow:hidden;width:128px;'>" + data.document.product[index].title_short + "</div>" +
+					"<div style='width:100%;height:100%;float:left;'>" +
+					"<div id='txt1' style='padding-top: 8px;/* padding-left:5px; */color: #ffffff;font-family:arial,sans-serif;font-size: 20px;/* height:14px; */overflow:hidden;width:145px;text-align:center'>"+ data.document.product[index].catzero.$t + " - " + data.document.product[index].manufacturer +"</div>" +
+					"<div id='txt3' style='padding-left:12px;padding-top: 8px;color:#f9f9f9;font-family:arial,sans-serif;font-size:65%;height:168px;overflow:hidden;width:128px;'>" + decodeHTMLEntities(data.document.product[index].title_short) +
+					"<div id='txt2' style='padding-left:5px;color: #ffffff;font-family:arial,sans-serif;font-size:24px;height:25px;overflow:hidden;width: 168px;margin-top: 9px;margin-left: 16px;text-align: left;'>" + data.document.product[index].direct_offer.price + "</div></div>" +
 					"</div>" +
 					"</div></a>" +
 					"</div>";
@@ -47,5 +48,53 @@ $(document).ready(function(){
 				return i;
 		}
 	}
+
+	function decodeHTMLEntities(text) {
+	    var entities = [
+	        ['apos', '\''],
+	        ['amp', '&'],
+	        ['lt', '<'],
+	        ['gt', '>'],
+	        ['#33','!'],
+			['#34','"'],
+			['#35','#'],
+			['#36','$'],
+			['#37','%'],
+			['#38','&'],
+			['#39','\''],
+			['#40','('],
+			['#41',')'],
+			['#42','*'],
+			['#43','+'],
+			['#44',','],
+			['#45','-'],
+			['#46','.'],
+			['#47','/'],
+			['#58',':'],
+			['#59',';'],
+			['#60','<'],
+			['#61','='],
+			['#62','>'],
+			['#63','?'],
+			['#64','@'],
+			['#91','['],
+			['#92','\\'],
+			['#93',']'],
+			['#94','^'],
+			['#95','_'],
+			['#96','`'],
+			['#97','-'],
+			['#123','{'],
+			['#124','|'],
+			['#125','}'],
+	    ];
+
+	    for (var i = 0, max = entities.length; i < max; ++i) 
+	        text = text.replace(new RegExp('&'+entities[i][0]+';', 'g'), entities[i][1]);
+
+	    return text;
+	}
+
+
 	});
 });
