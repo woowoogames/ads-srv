afbc56d1783ddac0b32f80cd1512b6d957010765
Ofer goli <Ofergolib@gmail.com>
2015-04-26 12:17:29 +0300

    new update delete banner ui

--- tools/tools.js
+++ tools/tools.js
@@ -1,4 +1,4 @@
-//var baseApi = require('../adsrv/baseapi');
+var baseApi = require('../adsrv/baseapi');
 var queryString = require('querystring');
 var _request = require('request');
 var url = require('url') ;
@@ -7,12 +7,11 @@ var tools = {
 		var queryObject = url.parse(request.url,true).query;
  	    var prms = queryString.stringify(queryObject);
 		var serviceUrl = "http://bi.montiera.com/coms/coms.asmx/"+request.params.fn+"?"+prms;
-
-		_request.get(serviceUrl).pipe(response);
-
-		/*baseApi.httpGetTimeout(serviceUrl,function(error,res,body) {
+		console.log(serviceUrl);
+		//_request.get(serviceUrl).pipe(response);
+		baseApi.httpGetTimeout(serviceUrl,function(error,res,body) {
 			response.end(body);
-		});*/
+		});
 	}
 }; 
 
