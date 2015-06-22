983120cdfbbd0ffac3d9cabbbe410350cca09118
yossieliyahu <yossi@montiera.com>
2015-06-22 12:12:15 +0300

    adding become feed to feeds.js

--- adsrv/data/feeds.js
+++ adsrv/data/feeds.js
@@ -1,360 +1,377 @@
-
 [
-    {
-        "name": "adworldmedia",
-        "type": "feed",
-        "requireSearchTerm": true,
-        "active": false,
-        "isSerp": false,
-        "https": false,
-        "handler": "./feeds/adworldmedia",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "us",
-                    "ca",
-                    "uk",
-                    "au",
-                    "nz"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "admarketplace",
-        "type": "feed",
-        "requireSearchTerm": true,
-        "active": false,
-        "isSerp": true,
-        "https": false,
-        "handler": "./feeds/admarketplace",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
+	{
+		"name": "adworldmedia",
+		"type": "feed",
+		"requireSearchTerm": true,
+		"active": false,
+		"isSerp": false,
+		"handler": "./feeds/adworldmedia",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"au",
+					"ca",
+					"nz",
+					"us"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "admarketplace",
+		"type": "feed",
+		"requireSearchTerm": true,
+		"active": false,
+		"isSerp": true,
+		"https": false,
+		"handler": "./feeds/admarketplace",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"us"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "matomy",
+		"type": "raw",
+		"requireSearchTerm": false,
+		"active": false,
+		"isSerp": false,
+		"https": false,
+		"handler": "./raw/matomy",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"au",
+					"at",
+					"ar",
+					"br",
+					"cl",
+					"co",
+					"de",
+					"es",
+					"fr",
+					"gb",
+					"hu",
+					"in",
+					"mx",
+					"pe",
+					"pl",
+					"ru",
+					"ve"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "become",
+		"type": "feed",
+		"requireSearchTerm": true,
+		"active": true,
+		"isSerp": false,
+		"https": false,
+		"handler": "./feeds/become",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
                     "us"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
                     "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "matomy",
-        "type": "raw",
-        "requireSearchTerm": false,
-        "active": false,
-        "isSerp": false,
-        "https": false,
-        "handler": "./raw/matomy",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "au",
-                    "at",
-                    "ar",
-                    "br",
-                    "cl",
-                    "co",
-                    "de",
-                    "es",
-                    "fr",
-                    "gb",
-                    "hu",
-                    "in",
-                    "mx",
-                    "pe",
-                    "pl",
-                    "ru",
-                    "ve"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "kelkoo",
-        "type": "feed",
-        "requireSearchTerm": true,
-        "active": true,
-        "isSerp": false,
-        "https": true,
-        "handler": "./feeds/kelkoo",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "de",
-                    "dk",
-                    "es",
-                    "fr",
-                    "it",
-                    "nl",
-                    "ru",
-                    "se"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "shopzilla",
-        "type": "feed",
-        "requireSearchTerm": true,
-        "active": true,
-        "isSerp": false,
-        "handler": "./feeds/shopzilla",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "de",
-                    "uk"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "pricegong",
-        "type": "feed",
-        "requireSearchTerm": true,
-        "active": false,
-        "isSerp": false,
-        "https": false,
-        "handler": "./feeds/pricegong",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "at",
-                    "au",
-                    "be",
-                    "br",
-                    "ca",
-                    "it",
-                    "es",
-                    "fr",
-                    "de",
-                    "nl",
-                    "ru",
-                    "dk",
-                    "no",
-                    "se",
-                    "gb",
-                    "in",
-                    "jp",
-                    "pl",
-                    "us"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "pricegrabber",
-        "type": "feed",
-        "requireSearchTerm": true,
-        "active": true,
-        "isSerp": false,
-        "https": true,
-        "handler": "./feeds/pricegrabber",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "us"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "shopping",
-        "type": "feed",
-        "requireSearchTerm": true,
-        "active": true,
-        "isSerp": false,
-        "https": false,
-        "handler": "./feeds/shopping",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "uk"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "firstoffer",
-        "type": "feed",
-        "requireSearchTerm": true,
-        "active": true,
-        "isSerp": false,
-        "https": false,
-        "handler": "./feeds/firstoffer",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "ddls",
-        "type": "ddls",
-        "requireSearchTerm": false,
-        "active": true,
-        "isSerp": false,
-        "https": false,
-        "handler": "./ddls/ddlmngr",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "trnds",
-        "type": "trnds",
-        "requireSearchTerm": false,
-        "active": true,
-        "isSerp": false,
-        "https": false,
-        "handler": "./trnds/trndmngr",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "buscape",
-        "type": "feed",
-        "requireSearchTerm": true,
-        "active": true,
-        "isSerp": false,
-        "https": false,
-        "handler": "./feeds/buscape",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "br",
-                    "ar",
-                    "co",
-                    "cl",
-                    "mx",
-                    "pe",
-                    "ve"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    },
-    {
-        "name": "buzzcity",
-        "type": "raw",
-        "requireSearchTerm": false,
-        "active": false,
-        "isSerp": false,
-        "https": false,
-        "handler": "./raw/buzzcity",
-        "coverage": {
-            "cntry": {
-                "type": "white",
-                "values": [
-                    "id",
-                    "my",
-                    "in",
-                    "za",
-                    "th",
-                    "br",
-                    "uk",
-                    "ng"
-                ]
-            },
-            "prdct": {
-                "type": "white",
-                "values": [
-                    "all"
-                ]
-            }
-        }
-    }
+				]
+			}
+		}
+	},
+	{
+		"name": "kelkoo",
+		"type": "feed",
+		"requireSearchTerm": true,
+		"active": true,
+		"isSerp": false,
+		"handler": "./feeds/kelkoo",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"dk",
+					"it",
+					"nl",
+					"ru",
+					"es",
+					"se"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "shopzilla",
+		"type": "feed",
+		"requireSearchTerm": true,
+		"active": true,
+		"isSerp": false,
+		"handler": "./feeds/shopzilla",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"fr",
+					"de",
+					"uk",
+					"us"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "pricegong",
+		"type": "feed",
+		"requireSearchTerm": true,
+		"active": false,
+		"isSerp": false,
+		"handler": "./feeds/pricegong",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"au",
+					"at",
+					"be",
+					"br",
+					"ca",
+					"dk",
+					"fr",
+					"de",
+					"in",
+					"it",
+					"jp",
+					"nl",
+					"no",
+					"pl",
+					"ru",
+					"es",
+					"se",
+					"us"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "pricegrabber",
+		"type": "feed",
+		"requireSearchTerm": true,
+		"active": true,
+		"isSerp": false,
+		"handler": "./feeds/pricegrabber",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"ca",
+					"uk",
+					"us"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "shopping",
+		"type": "feed",
+		"requireSearchTerm": true,
+		"active": true,
+		"isSerp": false,
+		"https": false,
+		"handler": "./feeds/shopping",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"uk"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "firstoffer",
+		"type": "feed",
+		"requireSearchTerm": true,
+		"active": false,
+		"isSerp": false,
+		"handler": "./feeds/firstoffer",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "ddls",
+		"type": "ddls",
+		"requireSearchTerm": false,
+		"active": true,
+		"isSerp": false,
+		"https": false,
+		"handler": "./ddls/ddlmngr",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "trnds",
+		"type": "trnds",
+		"requireSearchTerm": false,
+		"active": true,
+		"isSerp": false,
+		"https": false,
+		"handler": "./trnds/trndmngr",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "buscape",
+		"type": "feed",
+		"requireSearchTerm": true,
+		"active": true,
+		"isSerp": false,
+		"https": false,
+		"handler": "./feeds/buscape",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"br",
+					"ar",
+					"co",
+					"cl",
+					"mx",
+					"pe",
+					"ve"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	},
+	{
+		"name": "buzzcity",
+		"type": "raw",
+		"requireSearchTerm": false,
+		"active": false,
+		"isSerp": false,
+		"https": false,
+		"handler": "./raw/buzzcity",
+		"coverage": {
+			"cntry": {
+				"type": "white",
+				"values": [
+					"id",
+					"my",
+					"in",
+					"za",
+					"th",
+					"br",
+					"uk",
+					"ng"
+				]
+			},
+			"prdct": {
+				"type": "white",
+				"values": [
+					"all"
+				]
+			}
+		}
+	}
 ]
